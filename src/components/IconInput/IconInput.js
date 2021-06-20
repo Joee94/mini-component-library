c3660d179d234be2cc369f8b1476b6348e607da3
Joe Reddie <joee945@hotmail.co.uk>
2021-06-20 20:01:36 +0100

    dealt with this task a lot better - happy with the outcome for the most part

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+	small: {
+		'border-bottom': '1px solid black',
+		'height': '24px',
+		'padding-left': '24px',
+		'iconSize': '16px'
+	},
+	large: {
+		'border-bottom': '2px solid black',
+		'height': '36px',
+		'padding-left': '36px',
+		'iconSize': '24px'
+	}
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,49 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+	const styles = SIZES[size];
+  return( 
+  		<Wrapper >
+			  <VisuallyHidden>{label}</VisuallyHidden>
+			  <IconWrapper id={icon} size={styles.iconSize} />
+			  <TextInput type="text" name={label} placeholder={placeholder} style={styles} width={`${width}px`}/>
+		</Wrapper>
+	);
 };
+
+const IconWrapper = styled(Icon)`
+	position: absolute;
+	left: 0;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	height: ${p=>p.size};
+	width: ${p=>p.size};
+`
+
+const Wrapper = styled.div`
+	position: relative;
+`
+
+const TextInput = styled.input`
+	border-style: none;
+	color: ${COLORS.gray700};
+	font-weight: bold;
+	width: ${p => p.width};
+
+	&::placeholder {
+		font-weight: initial;
+		color: ${COLORS.gray500};
+	}
+
+	&:focus {
+		outline-offset: 3px;
+	}
+
+	&:hover {
+		color: ${COLORS.black};
+	}
+`
+
 
 export default IconInput;
