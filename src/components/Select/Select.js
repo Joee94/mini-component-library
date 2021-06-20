2cfa76475b651a27b28a0a902d74ab6e0f92ce0a
Joe Reddie <joee945@hotmail.co.uk>
2021-06-20 18:01:58 +0100

    completed select- had to use the video to get started and not very happy with outcome- feels very hacky

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,11 +8,47 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+  return ( 
+	<Wrapper>
+		<NativeDropdown value={value} onChange={onChange} >
+			{children} 
+		</NativeDropdown>
+		<ShownDropdown >
+			{displayedValue}
+			<IconWrapper id={'chevron-down'} width= {'12'}/>
+		</ShownDropdown>
+	</Wrapper>
   );
 };
+
+const IconWrapper = styled(Icon)`
+	position: absolute;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	padding: 8px;
+`
+
+
+const Wrapper = styled.div`
+	position: relative;
+	width: max-content;
+`
+
+const NativeDropdown = styled.select`
+	position: absolute;
+	opacity: 0;
+	width: 100%;
+	height: 100%;
+`
+
+const ShownDropdown = styled.div`
+	background-color: ${COLORS.transparentGray15};
+	border: none;
+	border-radius: 8px;
+	padding: 12px 36px 12px 16px; 
+	color: ${COLORS.gray700};
+`
+
 
 export default Select;
